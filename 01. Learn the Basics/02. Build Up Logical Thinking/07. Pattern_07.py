02133a10456957d720c2f95a1b10dfdd022fbe99
MedhajSarang <sarangmedhaj@gmail.com>
2026-01-25 09:41:07 +0530

    Solution to Problem 7 on Patterns

--- 01. Learn the Basics/02. Build Up Logical Thinking/07. Pattern_07.py
+++ 01. Learn the Basics/02. Build Up Logical Thinking/07. Pattern_07.py
@@ -11,3 +11,10 @@ Expected Output:-
 '''
 
 #Code:- 
+N = 5
+for i in range(1,N + 1):    #Number of Rows
+  for j in range(N - i):    #Spaces Before
+    print(" ", end=" ")     #Print the blank spaces
+  for j in range(2 * i - 1):#Number of stars per row
+    print("*", end=" ")     #Print the stars
+  print(" ")                #Move to next row
