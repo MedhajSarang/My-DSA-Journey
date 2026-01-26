f82f8a9ce357aa37a5d02da0ea00aabf5e8307a5
MedhajSarang <sarangmedhaj@gmail.com>
2026-01-26 14:29:10 +0530

    Solution to pattern problem 9 added

--- 01. Learn the Basics/02. Build Up Logical Thinking/09. Pattern_09.py
+++ 01. Learn the Basics/02. Build Up Logical Thinking/09. Pattern_09.py
@@ -16,13 +16,15 @@ Expected Output:-
 #Status - Still solving this one
 #Code
 N = 5
-for i in range(0, N*2):
-    for j in range(0, N - i - 1):
-        print(" ", end=" ")
-    for j in range(0, 2 * i - 1):
-        print("*",end=" ")
-    for j in range(0,i):
-        print(" ",end=" ")
-    for j in range(0, 2 * N - (2 * i + 1)):
-        print("*",end=" ")
-    print()
+for i in range(0, N):           #Code for top half
+  for j in range(0, N - i - 1): #Logic for blank spaces 
+    print(" ", end=" ")         #Printing the blank spaces
+  for j in range(0, 2*i + 1):   #Logic for stars in the top half
+    print("*",end=" ")          #Printing the stars
+  print()                       #Move to next row
+for i in range(N,0,-1):         #Code for bottom half
+  for j in range(0, N - i):     #Logic for blank spaces
+    print(" ", end=" ")         #Print blank spaces
+  for j in range(0, 2*i - 1):   #Logic for stars in the bottom half
+    print("*",end=" ")          #Print the stars in bottom half
+  print()                       #Move to next row
