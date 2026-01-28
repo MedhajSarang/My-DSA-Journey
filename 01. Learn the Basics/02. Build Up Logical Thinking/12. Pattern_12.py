eae76f9fd29df160af94412b03caa78bd567b766
MedhajSarang <sarangmedhaj@gmail.com>
2026-01-28 18:30:28 +0530

    Trying to solve the pattern problem 12

---
+++ 01. Learn the Basics/02. Build Up Logical Thinking/12. Pattern_12.py
@@ -0,0 +1,22 @@
+'''
+Problem 12:- Number Crown Pattern
+
+Expected Output:- 
+1             1
+1 2         2 1
+1 2 3     3 2 1
+1 2 3 4 4 3 2 1 
+'''
+
+#Code
+N = 5
+for i in range(1, N):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    for s in range(2 * (N - 1) + 1):
+        print(" ", end=" ")
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    
+    print()
+  
