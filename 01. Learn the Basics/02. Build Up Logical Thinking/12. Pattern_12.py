7fc66caccc3ae22b1d150e4bf046e9cac55c30be
MedhajSarang <sarangmedhaj@gmail.com>
2026-02-02 10:59:58 +0530

    Solution to pattern problem 12 added

--- 01. Learn the Basics/02. Build Up Logical Thinking/12. Pattern_12.py
+++ 01. Learn the Basics/02. Build Up Logical Thinking/12. Pattern_12.py
@@ -11,12 +11,11 @@ Expected Output:-
 #Code
 N = 5
 for i in range(1, N):               #Determine the number of rows
-    for j in range(1, i + 1):
-        print(j, end=" ")
-    for s in range(2 * (N - 1) + 1):
+    for j in range(1, i + 1):       #Logic for the left side
+        print(j, end=" ")           
+    for s in range(2 * (N - 1 - i)):#Logic for the spaces, using N - 1 here as N is 5 but the pattern maxes out at 4. 
         print(" ", end=" ")
-    for j in range(i, 0, -1):
+    for j in range(i, 0, -1):       #Logic to print to numbers on the right side in reverse order.
         print(j, end=" ")
-    
-    print()
+    print()                         #Move to then next line 
   
