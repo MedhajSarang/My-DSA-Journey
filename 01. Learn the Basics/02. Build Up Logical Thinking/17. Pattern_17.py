7da55c23102561212355680ad554a606624a9b63
MedhajSarang <sarangmedhaj@gmail.com>
2026-02-07 12:41:40 +0530

    Solution to pattern problem 17 added

--- 01. Learn the Basics/02. Build Up Logical Thinking/17. Pattern_17.py
+++ 01. Learn the Basics/02. Build Up Logical Thinking/17. Pattern_17.py
@@ -11,8 +11,15 @@ A B C D C B A
 
 #Code: 
 
-N = 5
-for i in range(N): 
-    print(" " * (N - i - 1), end=" ")
-    ch = chr(65 + i)
-    
+N = 4
+for i in range(N):                     #Total number of rows
+    print("  " * (N - i - 1), end=" ") #Logic to print the spaces before the pattern
+    ch = ord('A')                      #Returns the ordinal value of a character
+    breakpoint = (2 * i + 1) // 2
+    for j in range(1, 2 * i + 2):      #Logic for the inner loop
+        print(chr(ch), end=" ")        #Print the characters in the rows
+        if j <= breakpoint:            
+            ch += 1
+        else: 
+            ch -= 1
+    print()
