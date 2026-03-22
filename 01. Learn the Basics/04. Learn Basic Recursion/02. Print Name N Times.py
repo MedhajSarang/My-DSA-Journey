e7fdd81a36e6018bd1e9f187d708977e73a2ac11
MedhajSarang <sarangmedhaj@gmail.com>
2026-03-22 12:41:31 +0530

    Print Name N Times using Recursion

---
+++ 01. Learn the Basics/04. Learn Basic Recursion/02. Print Name N Times.py
@@ -0,0 +1,17 @@
+'''
+Print Name N times using Recursion
+
+Description: Given an integer N, write a program to print your name N times. 
+
+Example: 
+Input: N = 3
+Output:- Medhaj Medhaj Medhaj
+Explanation: Name is printed 3 times.
+'''
+
+#Code
+def printName(self, name, count, N):
+    if count == N:
+        return
+    print(Name)
+    self.printName(name, count + 1, N)
